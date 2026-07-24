f5d445c79d83da2eeb2c78dddc52589c7bf7383f
DALI <angepaterne.dali@gmail.com>
2026-07-25 00:57:03 +0200

    fix tailwind config

--- src/app/components/AppHeader.tsx
+++ src/app/components/AppHeader.tsx
@@ -1,6 +1,6 @@
 const AppHeader = () => {
      return (
-          <header>
+          <header className="sticky top-0 z-50 bg-primary border-b-4 px-5 py-3 flex items-center justify-between">
                <h1>HEADER</h1>
           </header>
      );
